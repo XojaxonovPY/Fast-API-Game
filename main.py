75fd58cf6dc401ee821b47a8bc6e9cdc5a140d39
User <asqarservis0000@gmail.com>
2025-07-31 19:55:53 +0300

    updated

--- main.py
+++ main.py
@@ -50,8 +50,7 @@ async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
 
-if __name__ == "__main__":
-    asyncio.run(init_models())
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token/")
 
